f1c839055f84efc06e5ec10e67981091fc2ffbc1
kaitokosuge <kaitokosuge.mi@gmail.com>
2023-09-10 15:31:00 +0900

    Menu追加

--- resources/js/Pages/Posts/Show.jsx
+++ resources/js/Pages/Posts/Show.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Authenticated from "@/Layouts/AuthenticatedLayout";
 import { Link, useForm } from '@inertiajs/react';
 import { useState } from "react";
+import Menu from "../Common/Menu";
 //import { FollowButton } from "../Button/FollowButton";
 //import InfiniteScroll from "react-infinite-scroller"
 
@@ -22,22 +23,7 @@ function Show( props ) {
                     <a href="" className="flex font-bold mt-5 items-center"><img className="w-[35px] mr-5"src="img/hand.png"/>friends</a>
                 </div>
                     <div class="border-t border-gray-300 mt-5">
-                        <div className="mt-5 font-bold text-md w-[45%] p-1"><a href="" className="flex"><img src="img/tora.png" className="w-[25px] mr-5"/><p>INTJ</p></a></div>
-                        <div className="mt-5 font-bold text-md w-[45%] p-1"><a href="" className="flex"><img src="img/hukuro.png"className="w-[25px] mr-5"/><p>INTP</p></a></div>
-                        <div className="mt-5 font-bold text-md w-[45%] p-1"><a href="" className="flex"><img src="img/lion.png"className="w-[25px] mr-5"/><p>ENTJ</p></a></div>
-                        <div className="mt-5 font-bold text-md w-[45%] p-1"><a href="" className="flex"><img src="img/oumu.png"className="w-[25px] mr-5"/><p>ENTP</p></a></div>
-                        <div className="mt-5 font-bold text-md w-[45%] p-1"><a href="" className="flex"><img src="img/kirin.png"className="w-[25px] mr-5"/><p>INFJ</p></a></div>
-                        <div className="mt-5 font-bold text-md w-[45%] p-1"><a href="" className="flex"><img src="img/usagi.png"className="w-[25px] mr-5"/><p>INFP</p></a></div>
-                        <div className="mt-5 font-bold text-md w-[45%] p-1"><a href="" className="flex"><img src="img/dog.png"className="w-[25px] mr-5"/><p>ENFJ</p></a></div>
-                        <div className="mt-5 font-bold text-md w-[45%] p-1"><a href="" className="flex"><img src="img/zou.png"className="w-[25px] mr-5"/><p>ENFP</p></a></div>
-                        <div className="mt-5 font-bold text-md w-[45%] p-1"><a href="" className="flex"><img src="img/kaba.png"className="w-[25px] mr-5"/><p>ISTJ</p></a></div>
-                        <div className="mt-5 font-bold text-md w-[45%] p-1"><a href="" className="flex"><img src="img/sika.png"className="w-[25px] mr-5"/><p>ISFJ</p></a></div>
-                        <div className="mt-5 font-bold text-md w-[45%] p-1"><a href="" className="flex"><img src="img/kuma.png"className="w-[25px] mr-5"/><p>ESTJ</p></a></div>
-                        <div className="mt-5 font-bold text-md w-[45%] p-1"><a href="" className="flex"><img src="img/uma.png"className="w-[25px] mr-5"/><p>ESFJ</p></a></div>
-                        <div className="mt-5 font-bold text-md w-[45%] p-1"><a href="" className="flex"><img src="img/cat.png"className="w-[25px] mr-5"/><p>ISTP</p></a></div>
-                        <div className="mt-5 font-bold text-md w-[45%] p-1"><a href="" className="flex"><img src="img/hituji.png"className="w-[25px] mr-5"/><p>ISFP</p></a></div>
-                        <div className="mt-5 font-bold text-md w-[45%] p-1"><a href="" className="flex"><img src="img/simauma.png"className="w-[25px] mr-5"/><p>ESTP</p></a></div>
-                        <div className="mt-5 font-bold text-md w-[45%] p-1"><a href="" className="flex"><img src="img/usi.png"className="w-[25px] mr-5"/><p>ESFP</p></a></div>
+                        <Menu/>
                     </div>
                 </div>
             <div class="flex justify-between w-[82%] ml-[18%]">
