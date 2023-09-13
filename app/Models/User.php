076030a3809f1226c46bce43bbba8a81100f0503
Kazuki-Iida <kazuki083177@gmail.com>
2023-09-14 00:50:50 +0900

    チャット周りのテーブルとメソッドを作成。

--- app/Models/User.php
+++ app/Models/User.php
@@ -94,7 +94,6 @@ class User extends Authenticatable
         return $this->belongsToMany(self::class, 'friends', 'user_id', 'friend_id');
     }
 
-    
 
     public function mbti()
     {
@@ -105,4 +104,15 @@ class User extends Authenticatable
     {
         return $this->hasMany(Post::class);
     }
+    
+    public function ownerTalks()
+    {
+        return $this->hasMany(Talk::class, 'owner_id')
+    }
+    
+    public function guestTalks()
+    {
+        return $this->hasMany(Talk::class, 'guest_id')
+    }
+
 }
